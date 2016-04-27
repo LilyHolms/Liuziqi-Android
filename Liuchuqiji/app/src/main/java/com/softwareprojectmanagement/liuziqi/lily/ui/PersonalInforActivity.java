4ce85db83179d60d12e62fb0b30531cb00c410e6
bigchestnut <lqq604350132@outlook.com>
2016-04-27 13:20:43 +0800

    登陆注册

--- Liuchuqiji/app/src/main/java/com/softwareprojectmanagement/liuziqi/lily/ui/PersonalInforActivity.java
+++ Liuchuqiji/app/src/main/java/com/softwareprojectmanagement/liuziqi/lily/ui/PersonalInforActivity.java
@@ -1,13 +1,26 @@
 package com.softwareprojectmanagement.liuziqi.lily.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-//9修改个人信息
-public class PersonalInforActivity extends AppCompatActivity {
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import butterknife.Bind;
+import butterknife.OnClick;
+//9修改个人信息
+public class PersonalInforActivity extends BaseActivity {
+    @Bind(R.id.btn_to_password)
+    Button btn_to_password;
+    @Bind(R.id.et_nickname)
+    EditText et_nickname;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_infor);
+
+    }
+    @OnClick(R.id.btn_to_password)
+    public void onToPasswordClick(View v){
+        startActivity(SetPasswordActivity.class,null,true);
     }
 }
